ccaa991d6c3c45050577ed7971863221bdfad34d
Laison <laisonmarko7@gmail.com>
2021-05-16 02:19:26 +0300

    feat :pdf read

--- alice.py
+++ alice.py
@@ -1,22 +1,21 @@
 import pyttsx3
-import wikipedia
-import speech_recognition as sr
 import datetime
+import wikipedia
+import PyPDF2
+import speech_recognition as sr
 import os
 import sys
 import smtplib
-import psutil
 import pyautogui
-import psutil
 import os
-import getpass
 import pyautogui
+
 
 
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[1].id) #select voice 0 male 1 female
 engine.setProperty('rate', 175)     # setting up new voice rate
 
 
@@ -69,6 +68,7 @@ def sendEmail(to, content):
     server.login('email', 'password')
     server.sendmail('email', to, content)
     server.close()
+    speak('mail sent')
 
 def screenshot():
     img = pyautogui.screenshot()
@@ -78,12 +78,38 @@ def getPath(command):
     with open('paths.txt') as f:
         for line in f:
             if command.lower() in line.lower():
-                a="start "+line #System command
-                print("start from a line")
-                os.system(a) #Opening the file
-                return True
+                return line
+                break
 
 
+def findFile(command):
+    path='C:/'
+    open('paths.txt', 'a')
+    i=1
+    for root,dirs,files in os.walk(path): #Lisiting all files
+     if i=1:
+        for entry in files: #Looking through files
+            if command.lower() in entry.lower(): #comparing searched file with available files
+                response=listen()
+                speak("Did u mean "+entry)
+                if "yes" in response.lower():
+                    
+                    print(entry)  #print founded file
+                    print("Opening"+root)
+                    root=root.replace('C:/','C:/"')
+                    root=root.replace('""','"') 
+                    root=root+'"'
+                    entry= '"'+entry+'"'
+                    a=os.path.join(root,entry) #Getting a full path of the file
+                    print(a) 
+                    # a= "'"+'"'+a+'"'+"'"
+                    # a=a.replace(' ','/') 
+                    history = open('paths.txt', 'a')
+                    history.write(a+"\n")
+                    history.close()
+                   
+                    i=0
+                    return(a)
 
 
 while True:
@@ -131,52 +157,63 @@ while True:
 
     elif 'open' in command:
         path='C:/'
+        print("in open")       
+        command=command.replace('open ','')     #Removing  open from String
+        open('paths.txt', 'a')
+        
+        if getPath(command) is not None:
+            path=getPath(command)
+            a="start "+path #System command
+            print("start from a line")
+            os.system(a) #Opening the file
+            findFile(command)
+        else if findFile(command) is not None:
+            path=getPath(command)
+            command="start "+command #System command
+            print(command)
+            os.system(command) #Opening the file
 
+        else:
+            speak("sorry i did not find the file")
 
-        if "open" in command.lower():
-            print("in open")       
-            command=command.replace('open ','')     #Removing  open from String
-            open('paths.txt', 'a')
-            
-            if not getPath(command):
-                for root,dirs,files in os.walk(path): #Lisiting all files
-                    for entry in files: #Looking through files
-                        if command.lower() in entry.lower(): #comparing searched file with available files
-                            response=listen()
-                            speak("Did u mean "+entry)
-                            if "yes" in response.lower():
-                                
-                                print(entry)  #print founded file
-                                print("Opening"+root)
-                                root=root.replace('C:/','C:/"')
-                                root=root.replace('""','"') 
-                                root=root+'"'
-                                entry= '"'+entry+'"'
-                                a=os.path.join(root,entry) #Getting a full path of the file
-                                print(a) 
-                                # a= "'"+'"'+a+'"'+"'"
-                                # a=a.replace(' ','/') 
-                                history = open('paths.txt', 'a')
-                                history.write(a+"\n")
-                                history.close()
-                                a="start "+a #System command
-                                print(a)
-                                os.system(a) #Opening the file
 
 
     elif 'read' in command:
+        path='C:/'
+        command=command.replace('read ','')
+        speak("search for a book with tittle  "+command)       
+          
+        if path=getPath(command) is not None:
+            
+            # speak("reading "+name)
+            pdfFileObject = open(path,'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
+            count = pdfReader.numPages
+            for i in range(count):
+                page = pdfReader.getPage(i)
+                speak(page.extractText())
+        else if path=findFile(command) is not None:
 
-        pdfFileObject = open(command,'rb')
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
-        count = pdfReader.numPages
-        for i in range(count):
-            page = pdfReader.getPage(i)
-            speak(page.extractText())
+            # speak("reading "+name)
+            pdfFileObject = open(path,'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
+            count = pdfReader.numPages
+            for i in range(count):
+                page = pdfReader.getPage(i)
+                speak(page.extractText())
+        else:
+            speak("sorry i did not find the file")
+        
 
 
     elif 'up' in command:
 
         pyautogui.press("up")
+
+
+    elif 'screenshot' in command:
+
+        screenshot()
 
     elif 'left' in command:
 
@@ -205,7 +242,7 @@ while True:
     elif 'your name' in command:
         speak('My name is Alice')
     elif 'stands for' in command:
-        speak('A.L.I.C.E stands for Automated Laison intelligent controll Entity')
+        speak('Alice stand for girlish name Alice')
    
 
     elif 'shutdown' in command:
@@ -228,22 +265,14 @@ while True:
         remember = open('data.txt', 'r')
         speak("you said me to remember that" + remember.read())
 
-    elif 'sleep' in command:
-        sys.exit()
-
-    elif 'dictionary' in command:
-        speak('What you want to search in your intelligent dictionary?')
-        translate(listen())
-
 
     elif 'email to' in command:
         try:
             command=command.replace('email to','')
             speak('What is the content?')
             content = listen()
-            to = command
-            sendEmail(to, content)
-            speak('Email has been sent!')
+            sendEmail(command, content)
+            speak('Email ent!')
 
         except Exception as e:
-            speak('Sorry sir, Not able to send email at the moment')
+            speak('Sending email not successfull')
